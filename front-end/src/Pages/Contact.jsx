932ff9109f1f90bd9d8f5143547292bb98aad9b8
Bhavesh Gagnani <142393452+Bhavesh-Gagnani@users.noreply.github.com>
2025-04-11 00:26:41 +0530

    Update Contact.jsx

--- front-end/src/Pages/Contact.jsx
+++ front-end/src/Pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await axios.post('http://localhost:5000/api/contact', formData);
+        const response = await axios.post('http://lms-six-silk.vercel.app//api/contact', formData);
         
         // Set message with green color for success
         setResponseMessage(
